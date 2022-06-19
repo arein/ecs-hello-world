9b2eb7015b259f9d3b83e14db2c52225f0c3fdcd
Derek <alexanderderekrein@gmail.com>
2022-06-19 17:31:25 -0400

    feat: deploys task

--- main.tf
+++ main.tf
@@ -16,6 +16,113 @@ resource "aws_ecr_repository" "ecs_hello_world" {
   name = "ecs-hello-world"
 }
 
+
+resource "aws_cloudwatch_log_group" "ecs_logs" {
+  name = "ecs_logs"
+  retention_in_days = 14
+}
+
 resource "aws_ecs_cluster" "app_cluster" {
   name = "app-cluster"
+
+  configuration {
+    execute_command_configuration {
+      logging    = "OVERRIDE"
+
+      log_configuration {
+        cloud_watch_encryption_enabled = false
+        cloud_watch_log_group_name     = aws_cloudwatch_log_group.ecs_logs.name
+      }
+    }
+  }
+}
+
+resource "aws_ecs_task_definition" "app_task" {
+  family                   = "app-task" # Naming our first task
+  container_definitions    = <<DEFINITION
+  [
+    {
+      "name": "app-task",
+      "image": "${aws_ecr_repository.ecs_hello_world.repository_url}",
+      "essential": true,
+      "portMappings": [
+        {
+          "containerPort": 3000,
+          "hostPort": 3000
+        }
+      ],
+      "memory": 512,
+      "cpu": 256,
+        "logConfiguration": {
+          "logDriver": "awslogs",
+          "options": {
+            "awslogs-group": "${aws_cloudwatch_log_group.ecs_logs.name}",
+            "awslogs-region": "us-east-1",
+            "awslogs-stream-prefix": "ecs"
+          }
+        }
+    }
+  ]
+  DEFINITION
+  requires_compatibilities = ["FARGATE"] # Stating that we are using ECS Fargate
+  network_mode             = "awsvpc"    # Using awsvpc as our network mode as this is required for Fargate
+  memory                   = 512         # Specifying the memory our container requires
+  cpu                      = 256         # Specifying the CPU our container requires
+  execution_role_arn       = "${aws_iam_role.ecsTaskExecutionRole.arn}"
+
+  runtime_platform {
+    operating_system_family = "LINUX"
+    cpu_architecture        = "ARM64"
+  }
+}
+
+resource "aws_iam_role" "ecsTaskExecutionRole" {
+  name               = "ecsTaskExecutionRole"
+  assume_role_policy = "${data.aws_iam_policy_document.assume_role_policy.json}"
+}
+
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
+  role       = "${aws_iam_role.ecsTaskExecutionRole.name}"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_ecs_service" "app_service" {
+  name            = "app-service"
+  cluster         = "${aws_ecs_cluster.app_cluster.id}"
+  task_definition = "${aws_ecs_task_definition.app_task.arn}"
+  launch_type     = "FARGATE"
+  desired_count   = 3 # Setting the number of containers we want deployed to 3
+
+  network_configuration {
+    subnets          = ["${data.aws_subnet.default_subnet_a.id}", "${data.aws_subnet.default_subnet_b.id}", "${data.aws_subnet.default_subnet_c.id}"]
+    assign_public_ip = true # Providing our containers with public IPs
+  }
+}
+
+data "aws_vpc" "selected" {
+  id = "vpc-0b419aaece54bc845"
+}
+
+# Providing a reference to our default subnets
+data "aws_subnet" "default_subnet_a" {
+  id = "subnet-019e3a148e85f7ac7"
+}
+
+data "aws_subnet" "default_subnet_b" {
+  id = "subnet-00657dc783026e9a2"
+}
+
+data "aws_subnet" "default_subnet_c" {
+  id = "subnet-06e834f14bed52b99"
 }
