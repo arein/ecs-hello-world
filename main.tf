fb7f46f599cabcebe0df6cae855b52e218df55ce
Derek <alexanderderekrein@gmail.com>
2022-06-19 19:46:40 -0400

    feat: adds cd

--- main.tf
+++ main.tf
@@ -72,7 +72,7 @@ resource "aws_ecs_task_definition" "app_task" {
 
   runtime_platform {
     operating_system_family = "LINUX"
-    cpu_architecture        = "ARM64"
+    # cpu_architecture        = "ARM64"
   }
 }
 
